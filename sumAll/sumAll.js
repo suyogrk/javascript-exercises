3a6d9cc23dd75954527d1fc0e9272bb92b36dffe
SuyogRK <suyong59@gmail.com>
2020-06-08 10:49:41 +0545

    sumall completed

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (initialNumber, finalNumber) {
+    let startNum, endNum;
 
+    if(!Number.isInteger(initialNumber) || !Number.isInteger(finalNumber)){
+        return 'ERROR'
+    }
+
+    if(initialNumber<0 || finalNumber<0){
+        return 'ERROR'
+    }
+
+    if (initialNumber > finalNumber) {
+        startNum = finalNumber;
+        endNum = initialNumber;
+    } else {
+        startNum = initialNumber;
+        endNum = finalNumber;
+    }
+
+    let sum = 0;
+    for (let i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 }
 
 module.exports = sumAll

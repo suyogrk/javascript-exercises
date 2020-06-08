bf4ac74ef2b611274fca354e161f100b21ebf991
SuyogRK <suyong59@gmail.com>
2020-06-08 09:52:08 +0545

    remove from array completed

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(initialArray, ...itemToBeRemoved) {
 
+    const arrayRemoved = initialArray.filter((item) =>{        
+        return !itemToBeRemoved.includes(item);
+    } )
+
+return arrayRemoved;
 }
 
 module.exports = removeFromArray

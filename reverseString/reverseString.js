bea43dd86833bbe1e30bdeea14e1894558e0de60
SuyogRK <suyong59@gmail.com>
2020-06-08 09:01:13 +0545

    completed reversestring

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(stringToBeReversed) {
+    const stringLength = stringToBeReversed.length;
+    let reversedString = ''
+
+    for(let i=stringLength-1; i>=0; i--){
+        let tmpChar = stringToBeReversed.charAt(i);
+
+        reversedString += tmpChar;
+
+    }
+
+    return reversedString;
 
 }
-
 module.exports = reverseString

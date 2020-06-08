6dcdc1ff43d0d6a6aed0bf7b4ecd976c6d977804
SuyogRK <suyong59@gmail.com>
2020-06-08 08:52:20 +0545

    repeatstring completed

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (tmpString, numberOfRepetions) {
+    
+    if(numberOfRepetions<0){
+        return 'ERROR'
+    }
 
+    let responseString = '';
+
+    for (let i = 0; i < numberOfRepetions; i++) {
+        responseString += tmpString
+
+    }
+
+    return responseString;
 }
 
 module.exports = repeatString

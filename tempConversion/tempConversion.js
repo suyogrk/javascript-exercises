7421f2f892627b0add8c05db107e72e03417a0a7
SuyogRK <suyong59@gmail.com>
2020-06-08 12:01:26 +0545

    tempconversion complete

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (tempInF) {
+  const tempInC = (tempInF - 32) * (5 / 9);
+  return Math.round(tempInC * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function (tempInC) {
+  const tempInF = (tempInC * (9 / 5)) + 32;
+  return Math.round(tempInF * 10) / 10;
 }
-
+s
 module.exports = {
   ftoc,
   ctof
